f82ea4b1e05e9bf1dc493afc2d377f612d293ebd
James Thibaudeau <jamesthibaudeau@gmail.com>
2016-12-15 20:42:15 +0000

    added jquery

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,12 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
     ],
     module: {
         loaders: [
