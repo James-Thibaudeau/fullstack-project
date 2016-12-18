5edee5e798ed4a6a0b87398c835db5d8abb5f7e2
James Thibaudeau <jamesthibaudeau@gmail.com>
2016-12-18 20:20:09 +0000

    added registerHandler, not working yet

--- client/actions/RegisterActions.js
+++ client/actions/RegisterActions.js
@@ -1,1 +1,19 @@
 //Registration actions
+import axios from 'axios';
+
+export function registerHandler(username, password) {
+    return dispatch => {
+        console.log('Sending registration request...');
+        axios.post('https://fullstack-project-jamesthibaudeau.c9users.io/api/auth/register', {
+            username,
+            password
+          })
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    };
+    
+}
