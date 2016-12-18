d2853bdb4badd5b3eb7a78afc59276c7c40b4ed1
James Thibaudeau <jamesthibaudeau@gmail.com>
2016-12-18 20:02:48 +0000

    added local state and mock submit function

--- client/components/Register/RegisterForm.js
+++ client/components/Register/RegisterForm.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import { Button, Col, ControlLabel, Form, FormControl, FormGroup, Panel } from 'react-bootstrap';
 
+
 class Register extends React.Component {
     
-    renderFormGroup(type) {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+        };
+    }
+    
+    //this is testing
+    submit(e) {
+        e.preventDefault();
+        console.log('submitting');
+    }
+    
+    setUsername(e) {
+        this.setState({
+            username: e.target.value,
+        });
+
+    }
+
+    setPassword(e) {
+        this.setState({
+            password: e.target.value,
+        });
+
+    }
+    
+    renderFormGroup(type, func) {
         return (
             <FormGroup controlId={'formHorizontal' + type}>
                 <Col componentClass={ControlLabel} sm={2}>
                     {type}
                 </Col>
                 <Col sm={10}>
-                    <FormControl type={type} placeholder={type} />
+                    <FormControl onChange={func} type={type} placeholder={type} />
                 </Col>
             </FormGroup>
             );
@@ -21,9 +50,9 @@ class Register extends React.Component {
 
                 <Col xs={12} md={6} mdOffset={3} lg={6} lgOffset={3}>
                     <Panel header={<h3>{this.props.type}</h3>} bsStyle="default">
-                        <Form horizontal>
-                            {this.renderFormGroup('Username')}
-                            {this.renderFormGroup('Password')}
+                        <Form horizontal onSubmit={this.submit.bind(this)}>
+                            {this.renderFormGroup('Username', this.setUsername.bind(this))}
+                            {this.renderFormGroup('Password', this.setPassword.bind(this))}
                             <FormGroup>
                                 <Col smOffset={2} sm={10}>
                                     <Button bsStyle="primary" type="submit">{this.props.type}</Button>
