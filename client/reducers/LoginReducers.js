db7b14f485a687f4f5808ae291e326688e2f1651
James Thibaudeau <jamesthibaudeau@gmail.com>
2016-12-19 18:28:58 +0000

    added logout reducers

--- client/reducers/LoginReducers.js
+++ client/reducers/LoginReducers.js
@@ -1,16 +1,19 @@
 //login reducers
 import { Map } from 'immutable';
-import { LOGIN, LOGIN_FAIL } from '../actions/LoginActions';
+import { LOGIN, LOGIN_FAIL, LOGOUT, LOGOUT_FAIL } from '../actions/LoginActions';
 
 const initialState = {user: {}, isLoggedIn: false, message:''};
 
 export default function (state = initialState, action) {  
-  console.log('reducer fired');
   switch(action.type) {
     case LOGIN:
       return {user: action.user, isLoggedIn: action.isLoggedIn, message: action.message};
     case LOGIN_FAIL:
       return {user: action.user, isLoggedIn: action.isLoggedIn, message: action.message};
+    case LOGOUT:
+      return {user: action.user, isLoggedIn: action.isLoggedIn, message: action.message};
+    case LOGOUT_FAIL:
+      return {message: action.message};
   }
   return state;
 }
