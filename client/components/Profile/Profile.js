44a509e311a55191df8bf259d089040cc8eac75e
James Thibaudeau <jamesthibaudeau@gmail.com>
2016-12-20 20:08:33 +0000

    modified profile page

--- client/components/Profile/Profile.js
+++ client/components/Profile/Profile.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Panel, Button} from 'react-bootstrap';
+import {Panel, Row, Col, Image, Button} from 'react-bootstrap';
 
 class Profile extends React.Component {
+    
+    renderRow(field, value){
+        return (
+             <Row>
+                <Col xs={12} sm={4} smOffset={2}>
+                    <p>{field}</p>
+                </Col>
+                <Col xs={12} sm={4} smOffset={2}>
+                    <p>{value || 'none'}</p>
+                </Col>
+            </Row>
+            );
+    }
     
     render() {
         return (
             <div className="container">
                 <Panel>
-                    Profile page
+                    <Row>
+                    <Col xs={12} sm={2}>
+                        <Image style={{height: 'auto', width: 'auto'}} src="/static/appointmentImage.png" responsive />
+                    </Col>
+                    <Col xs={12} sm={10}>
+                        <h1>{this.props.login.user.username}'s Profile</h1>
+                    </Col>
+                    <Row>
+                    <Col xs={12}>
+                    <hr />
+                    </Col>
+                    </Row>
+                    </Row>
+                    {this.renderRow('First Name:', this.props.login.user.firstName)}
+                    {this.renderRow('Last Name:', this.props.login.user.lastName)}
                 </Panel>
             </div>
         );
@@ -16,7 +43,7 @@ class Profile extends React.Component {
 };
 
 function mapStateToProps(state) {
-    return {};
+    return {...state};
 };
 
 export const ProfileContainer = connect(mapStateToProps)(Profile);
