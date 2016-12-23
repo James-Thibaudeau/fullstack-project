126c614b834ad34b873d1c62266f674bff9b1de4
James Thibaudeau <jamesthibaudeau@gmail.com>
2016-12-23 00:33:30 +0000

    added user reducers

--- client/reducers/RootReducer.js
+++ client/reducers/RootReducer.js
@@ -3,13 +3,15 @@ import { routerReducer } from 'react-router-redux';
 import RegisterReducers from './RegisterReducers';
 import LoginReducers from './LoginReducers';
 import AppointmentReducers from './AppointmentReducers';
+import UserReducers from './UserReducers';
 
 const rootReducer = combineReducers(
     {   
         routing: routerReducer,
         register: RegisterReducers,
         login: LoginReducers,
-        appointments: AppointmentReducers
+        appointments: AppointmentReducers,
+        users: UserReducers
     });
 
 export default rootReducer;
